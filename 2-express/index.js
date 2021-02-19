74f2d8b6a0696024ae6079af6abb8d9f5bc84195
Igna Garcia <naachxp@gmail.com>
2021-02-18 21:07:26 -0300

    get users and post user

--- 2-express/index.js
+++ 2-express/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+const userList = require('./data').users;
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-    res.write("some");
+    res.write("Ejecucion correcta");
     res.end();
 });
 
-app.get("/user", (req, res) => {
-    res.write(`user`);
+app.get("/users", (req, res) => {
+    res.status(200).json(userList)
+    res.end();
+});
+
+app.post("/users", async (req, res) => {
+    if(req.body && Object.keys(req.body).length > 0) {
+        await userList.push(req.body);
+        res.status(200).json(req.body);
+    }
+    else res.status(400).json({ message: "No se encuentra el Usuario" })
+
     res.end();
 });
 
@@ -19,4 +33,4 @@ router.get("/:name", (req, res) => {
 });
 
 app.use('/user', router);
-app.listen(8000);
+app.listen(8000, () => console.log("Server is ON in http://localhost:8000"));
