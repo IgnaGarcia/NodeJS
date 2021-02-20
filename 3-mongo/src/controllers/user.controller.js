22beb0f7bef09b2fcc75572440d67938769ce748
Igna Garcia <naachxp@gmail.com>
2021-02-19 21:07:57 -0300

    fixed http methods

--- 3-mongo/src/controllers/user.controller.js
+++ 3-mongo/src/controllers/user.controller.js
@@ -1,54 +1,71 @@
 const User = require('../models/user.model');
 
 const getUserList = (req, res) => {
-    return res.status(200).json({
-        message: "Users retrieved successfully",
-        data: userList
-    });
-}
-
-const getUser = (req, res) => {
-    const { id = null } = req.params;
-    if(userList[id]) return res.status(200).json({
-        message: "User retrieved successfully",
-        data: userList[id]
-    });
-    else return res.status(400).json({ message: `User ${id} not found` });
+    User.find({}).exec((err, users) => {
+        if(err) return res.status(500).json({ message: "Internal Server Error"});
+        if(!users) return res.status(404).json({ message: "Users not found"});
+        return res.status(200).json({
+            message: "Users retrieved successfully",
+            data: users
+        });
+    }); 
 }
 
 const createUser = (req, res) => {
     if(req.body && Object.keys(req.body).length > 0) {
-        let user = new User();
-        [user.username, user.age] = req.body;
+        let user = new User(req.body);
 
-        return res.status(201).json({
-            message: "User created successfully",
-            data: user
+        user.save((err, userStored) => {
+            if(err) return res.status(500).json({ message: "Internal Server Error"});
+            if(!userStored) return res.status(404).json({ message: "User not created"});
+            return res.status(201).json({
+                message: "User created successfully",
+                data: user
+            });
         });
     }
     else return res.status(400).json({ message: "User not received" });
 }
 
+const getUser = (req, res) => {
+    const { id = null } = req.params;
+
+    User.findById(id, (err, user) => {
+        if(err) return res.status(500).json({ message: "Internal Server Error"});
+        if(!user) return res.status(404).json({ message: `User ${id} not found` });
+        return res.status(200).json({
+            message: "User retrieved successfully",
+            data: user
+        });
+    });
+}
+
 const updateUser = (req, res) => {
     const { id = null} = req.params;
-    if(!userList[id]) return res.status(404).json({ message : `User ${id} not found` });
-    else if(req.body && Object.keys(req.body).length > 0) {
-        userList[id] = req.body;
-        return res.status(200).json({
-            message: "User updated successfully",
-            data: req.body
+
+    if(req.body && Object.keys(req.body).length > 0) {
+        const update = req.body;
+
+        User.findByIdAndUpdate(id, update, { new: true }, (err, userUpdated) => {
+            if(err) return res.status(500).json({ message: "Internal Server Error"});
+            if(!userUpdated) return res.status(404).json({ message: `User ${id} not found` });
+            return res.status(200).json({
+                message: "User updated successfully",
+                data: userUpdated
+            });
         });
     }
-    else return res.status(400).json({ message : "User not received" });
+    else return res.status(400).json({ message: "User not received" });
 }
 
 const deleteUser = (req, res) => {
     const { id = null} = req.params;
-    if(!userList[id]) return res.status(404).json({ message : `User ${id} not found` });
-    else{
-        userList = userList.filter((_user, index) => index!= id);
-        return res.status(204).json({ message : `User ${id} deleted successfully`});
-    }
+
+    User.findByIdAndDelete(id, (err, userDeleted) => {
+        if(err) return res.status(500).json({ message: "Internal Server Error"});
+        if(!userDeleted) return res.status(404).json({ message: `User ${id} not found` });
+        return res.status(204).json({ message: `User ${id} deleted successfully` });
+    });
 }
 
 module.exports = {
--- 3-mongo/src/routes/user.routes.js
+++ 3-mongo/src/routes/user.routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 
 router.route('/users')
